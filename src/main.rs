d9f5515cd7e5c6de4bb7da00d3d3129fff2f7c86
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-11-17 19:25:28 -0300

    Programa principal.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,25 @@
 use yew::prelude::*;
+use yew_router::prelude::*;
+use plante::components::{
+    header::Header,
+};
+use plante::routes::{
+    login::Login,
+    Route,
+    switch,
+};
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <BrowserRouter>
+            <Header>
+            {"HI"}
+            </Header>
+            <Switch<Route> render={Switch::render(switch)}/>
+        </BrowserRouter>
+    }
+}
 
 fn main() {
     yew::start_app::<App>();
