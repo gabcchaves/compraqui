6e7a9727b8fc70408a26bd3d73a5f48a42a65d23
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-11-20 16:49:44 -0300

    Roteamento da seção de cadastro.

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,8 +1,10 @@
-use crate::routes::login::Login;
 use yew::prelude::*;
 use yew_router::prelude::*;
+use crate::routes::login::Login;
+use crate::routes::register::Register;
 
 pub mod login;
+pub mod register;
 
 #[derive(Clone, Routable, PartialEq)]
 pub enum Route {
@@ -26,21 +28,19 @@ pub fn switch(routes: &Route) -> Html {
             <h1>{"Página Inicial"}</h1>
         },
         Route::Login => html! {
-            <>
-                <Login/>
-            </>
-        },
-        Route::NotFound => html! {
-            <h1>{"Não encontrado"}</h1>
+            <Login/>
         },
         Route::Register => html! {
-            <h1>{"Cadastro"}</h1>
+            <Register/>
         },
         Route::Cart => html! {
             <h1>{"Carrinho"}</h1>
         },
         Route::Explore => html! {
             <h1>{"Explorar"}</h1>
+        },
+        Route::NotFound => html! {
+            <h1>{"Não encontrado"}</h1>
         },
     }
 }
